ff0f47c54a6a7bb3b1ee4082db0742a279ae2ed4
TuiLaLamNe <125855357+TuiLaLamNe@users.noreply.github.com>
2023-03-26 12:34:24 +0700

    upload lan 2

--- product.php
+++ product.php
@@ -1,7 +1,7 @@
 
 <?php
 include("server.php");
-$db_handle = new DBController();
+$db_handle = new DBControler();
 if(!empty($_GET["action"])) {
     switch($_GET["action"]) {
         case "add":
@@ -95,4 +95,92 @@ if(!empty($_GET["action"])) {
             ?>
         <div>
     </div>
-   
+    <div id="shopping-cart">
+        
+        <?php include('noti.php')?>
+	    <?php if (isset($_SESSION['success'])) : ?>
+          <div class=" success" >
+          	<p>
+              <?php 
+              	echo $_SESSION['success']; 
+              	unset($_SESSION['success']);
+              ?>
+          	</p>
+          </div>	
+  	    <?php endif ?>
+	      <?php if (isset($_SESSION['error'])) : ?>
+          <div class="error" >
+          	<p>
+              <?php 
+              	echo $_SESSION['error']; 
+              	unset($_SESSION['error']);
+              ?>
+          	</p>
+          </div>
+  	    <?php endif ?>
+	  
+        <a id="btnEmpty" href="product.php?action=empty">Empty Cart</a>
+        <?php
+            if(isset($_SESSION["cart_item"])){
+                $total_quantity = 0;
+                $total_price = 0;
+        ?>	
+        <div style="align:center" class="container table-responsive" >
+            <h3>Order Details</h3>
+            <table class="table table-bordered" cellpadding="10" cellspacing="1">
+                <tbody>
+                    <tr>
+                        <th style="text-align:left;">Name</th>
+                        <th style="text-align:left;">Code</th>
+                        <th style="text-align:right;" width="5%">Quantity</th>
+                        <th style="text-align:right;" width="10%">Unit Price</th>
+                        <th style="text-align:right;" width="10%">Price</th>
+                        <th style="text-align:center;" width="5%">Remove</th>
+                    </tr>	
+                    <?php		
+                        foreach ($_SESSION["cart_item"] as $item){
+
+                    ?>
+                        <tr>
+                            <td><?php echo $item["name"]; ?></td>
+                            <td><?php echo $item["code"]; ?></td>
+                            <td style="text-align:right;"><?php 
+                            if($item["quantity"] < $item["amount"] ){
+                                echo $item["quantity"];
+                            } else {
+                                $item["quantity"] = $item["amount"];
+                                echo $item["quantity"];
+                            }
+                                $item_price = $item["quantity"]*$item["price"];
+                            ?></td>
+                            <td  style="text-align:right;"><?php echo $item["price"]." BNN$"; ?></td>
+                            <td  style="text-align:right;"><?php echo $item_price." BNN$"; ?></td>
+                            <td style="text-align:center; font-color:red;"><a href="product.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction" >Remove Item</a></td>
+                        </tr>
+                    <?php
+                            $total_quantity += $item["quantity"];
+                            $total_price += ($item["price"]*$item["quantity"]);
+
+                            }
+                            $_SESSION["price"] = $total_price;
+                    ?>
+                    <tr>
+                        <td colspan="2" align:="right">Total:</td>
+                        <td align:="right"><?php echo $total_quantity; ?></td>
+                        <td align:="right" colspan="2"><strong><?php echo number_format($total_price, 2)." BNN$"; ?></strong></td>
+                        <td style="text-align:center;"><a href="payment.php" class="">Buy now!!</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>		
+        <?php
+            } else {
+        ?>
+            <div class="no-records">Your Cart is Empty</div>
+        <?php 
+            }
+        ?>
+    </div>
+
+</body>
+</html>
